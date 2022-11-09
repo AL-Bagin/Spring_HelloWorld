1a6e84aad325a5109870c40fea31930bfa9224c4
AL-Bagin <mr.alex.bagin@mail.ru>
2022-11-09 23:16:02 +0300

    First commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение ссылок для HelloWorld: ");
+        System.out.println(bean1 == bean2);
+        System.out.println("Сравнение ссылок для Cat: ");
+        System.out.println(catBean1 == catBean2);
+
+        applicationContext.close();
+
     }
 }
